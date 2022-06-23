33a1822d41b82c203dacc1d2ec0282bcc8ae366d
Thiago Nunes Miziara <thiagonmiziara@gmail.com>
2022-06-22 21:09:35 -0300

    aula-03

--- src/pages/Event/Event.tsx
+++ src/pages/Event/Event.tsx
@@ -1,13 +1,16 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../../components/Header/Header";
 import { PLayerVideo } from "../../components/PlayerVideo/PLayerVideo";
 import { Sidebar } from "../../components/Sidebar/Sidebar";
 
 export const Event = () => {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
       <main className='flex flex-1'>
-        <PLayerVideo />
+        {slug ? <PLayerVideo lessonSlug={slug} /> : <div className='flex-1' />}
         <Sidebar />
       </main>
     </div>
